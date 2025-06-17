f16cc23cde328f3bfb84cc3ae068f3bf76116c11
null221 <agd1923@gmail.com>
2025-06-17 13:09:52 +0200

    added new property imageUrl

--- backend/src/Entity/Product.php
+++ backend/src/Entity/Product.php
@@ -22,6 +22,9 @@ class Product
     #[ORM\Column]
     private ?int $stock = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $imageUrl = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -59,6 +62,18 @@ class Product
     public function setStock(int $stock): static
     {
         $this->stock = $stock;
+
+        return $this;
+    }
+
+    public function getImageUrl(): ?string
+    {
+        return $this->imageUrl;
+    }
+
+    public function setImageUrl(?string $imageUrl): static
+    {
+        $this->imageUrl = $imageUrl;
 
         return $this;
     }
