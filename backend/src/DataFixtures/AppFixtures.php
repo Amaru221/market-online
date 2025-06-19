38ddce4242c9e5d949711f3110e9382aec531a74
Amaru221 <agd1923gmail.com>
2025-06-19 15:13:31 +0200

    updated backend

--- backend/src/DataFixtures/AppFixtures.php
+++ backend/src/DataFixtures/AppFixtures.php
@@ -10,7 +10,6 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-
     private UserPasswordHasherInterface $passwordHasher;
 
     public function __construct(UserPasswordHasherInterface $passwordHasher)
@@ -29,14 +28,19 @@ class AppFixtures extends Fixture
 
         $manager->persist($user);
 
-
-
-        foreach (range(1, 10) as $i) {
+        // Crea 100 productos
+        foreach (range(1, 100) as $i) {
             $product = new Product();
             $product->setName("Producto $i");
             $product->setPrice(mt_rand(10, 100));
             $product->setStock(mt_rand(0, 50));
-            $product->setImageUrl("https://via.placeholder.com/150");
+
+            // Imagen aleatoria con ID específica
+            // Picsum asegura una imagen única por ID
+            $imageId = 100 + $i; // Para evitar IDs demasiado bajos
+            $imageUrl = "https://picsum.photos/id/$imageId/300/200"; // Tamaño personalizable
+            $product->setImageUrl($imageUrl);
+
             $manager->persist($product);
         }
 
--- backend/src/Entity/Product.php
+++ backend/src/Entity/Product.php
@@ -7,7 +7,9 @@ use App\Repository\ProductRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
-#[ApiResource()]
+#[ApiResource(
+    paginationItemsPerPage: 12
+)]
 class Product
 {
     #[ORM\Id]
